48ebebeef5f1cf9d309d5283f83caea64ed0c080
kaldan007 <kaldantenzin@gmail>
2021-06-28 20:47:24 +0530

    transfer marker

--- test_serilaize.py
+++ test_serilaize.py
@@ -5,7 +5,12 @@ from openpecha.formatters import HFMLFormatter
 from openpecha.serializers import HFMLSerializer
 
 if __name__ == "__main__":
-    opf_path = "./opfs/P000999/P000999.opf"
-    hfml_path = "./hfmls/"
-    serializer = HFMLSerializer(opf_path, text_id="D1346")
+    hfml_path = "./"
+    opfs_path = "./opf/"
+    hfml_text = "./P000888/"
+    pecha_id = "P000888"
+    opf_path = f"./opf/{pecha_id}/{pecha_id}.opf"
+    # formatter = HFMLFormatter(output_path=opfs_path)
+    # formatter.create_opf(hfml_text, pecha_id)
+    serializer = HFMLSerializer(opf_path, text_id="D1119")
     serializer.serialize(output_path=hfml_path)
